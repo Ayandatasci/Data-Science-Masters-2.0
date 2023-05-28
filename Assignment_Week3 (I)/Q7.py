395b3c29f569eef0462cb7c996ef8bb854c9279c
Ayan Biswas <112680043+Ayandatasci@users.noreply.github.com>
2023-05-27 22:49:57 -0700

    Delete Assignment_Week3 (I) directory

--- Assignment_Week3 (I)/Q7.py
+++
@@ -1,5 +0,0 @@
-'''Q7. Write a List Comprehension to iterate through the given string: ‘pwskills’.
-Expected output: ['p', 'w', 's', 'k', 'i', 'l', 'l', 's']'''
-
-s="pwskills"
-print(list(map(lambda s : s, s)))
--- Assignment_Week3 (I)/Q9.py
+++
@@ -1,6 +0,0 @@
-'''Q9. Write a code to print odd numbers from 1 to 100
-         using list comprehension.'''
-
-numbers = list(range(101))
-odd_no = list(filter(lambda x : x%2 != 0, numbers))
-print(odd_no)
